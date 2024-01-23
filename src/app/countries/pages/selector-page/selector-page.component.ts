c7a8e4daafd1f9cda665c23a7598b9544a4872d7
Abel Borit <abelboritguitton@gmail.com>
2024-01-23 15:31:33 -0500

    [FEAT] reactive forms - FormBuilder service & nonNullable & HTML update

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class SelectorPageComponent {
+  constructor(private formBuilder: FormBuilder) {}
 
+  public myForm: FormGroup = this.formBuilder.nonNullable.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    borders: ['', Validators.required], // fronteras con los cuales cada país limita
+  });
 }
