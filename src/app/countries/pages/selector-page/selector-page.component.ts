4d0bce7989d912e0f22de12043271caa4765f728
Abel Borit <abelboritguitton@gmail.com>
2024-01-26 10:07:39 -0500

    [FEAT] selector-page.component

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-selector-page',
@@ -7,11 +9,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [],
 })
 export class SelectorPageComponent {
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private countriesService: CountriesService
+  ) {}
 
   public myForm: FormGroup = this.formBuilder.nonNullable.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
     borders: ['', Validators.required], // fronteras con los cuales cada país limita
   });
+
+  /* con eso se está usando el servicio CountriesService que es privado y sus propiedades como en este caso. Hacerlo de esta forma no afecta por crear nuevos espacios en memoria ya que esto apunta al espacio en memoria que ya tiene this.countriesService.getRegions, es decir, que this.countriesService.getRegions pasa por referencia para poder utilizarlo aquí */
+  get getRegions(): Region[] {
+    return this.countriesService.getRegions;
+  }
 }
